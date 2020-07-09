d970dcb1e6fb2bfb586a242c65623213b70f875b
Yu Ishii <asobuishii@gmail.com>
2020-07-09 23:57:53 +0900

    Fix lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,5 +6,11 @@ module.exports = {
   rules: {
     'function-paren-newline': 0,
     'object-curly-newline': 0,
+    'import/no-extraneous-dependencies': [
+      2,
+      {
+        devDependencies: ['**/*.spec.js'],
+      },
+    ],
   },
 };
