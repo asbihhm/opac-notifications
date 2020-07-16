82499c88d4cd960c84622c0f3be1101c0e4c3be6
Yu Ishii <asobuishii@gmail.com>
2020-07-17 01:14:07 +0900

    Bump version to 0.7.1

--- nix/node-packages.nix
+++ nix/node-packages.nix
@@ -314,7 +314,7 @@ let
   args = {
     name = "opac-notifications";
     packageName = "opac-notifications";
-    version = "0.7.0";
+    version = "0.7.1";
     src = ./..;
     dependencies = [
       sources."@slack/types-1.7.0"
