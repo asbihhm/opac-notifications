6a309295f3809db79d92d4d081dc04389338320f
asbish <asobuishii@gmail.com>
2021-02-25 03:23:09 +0900

    Update node2nix

--- generate.sh
+++ generate.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 node2nix -14 --lock ./package-lock.json \
-         --output nix/node-packages.nix \
-         --node-env nix/node-env.nix \
-         --composition nix/node-composition.nix
+  --output nix/node-packages.nix \
+  --node-env nix/node-env.nix \
+  --composition nix/node-composition.nix
